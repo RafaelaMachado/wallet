b3ef14b64cfb39646f7d6b33098cf2f2fbaf72b2
Rafaela <rafaela@operand.com.br>
2019-09-24 10:34:32 -0300

    Created initial structure of the principal views

--- src/router.js
+++ src/router.js
@@ -1,10 +1,34 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
+const Wallet = () => import('@/views/Wallet' /* webpackChunkName: "wallet" */)
+const Login = () => import('@/views/Login' /* webpackChunkName: "login" */)
+const Register = () => import('@/views/Register' /* webpackChunkName: "register" */)
+
 Vue.use(Router)
 
 export default new Router({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes: []
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes: [
+        {
+            name: 'wallet',
+            path: '/',
+            component: Wallet
+        },
+        {
+            name: 'login',
+            path: '/login',
+            component: Login
+        },
+        {
+            name: 'register',
+            path: '/register',
+            component: Register
+        },
+        {
+            path: '*',
+            redirect: '/'
+        }
+    ]
 })
---
+++ src/views/Wallet.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        Wallet
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Wallet'
+}
+</script>
+
+<style>
+</style>
