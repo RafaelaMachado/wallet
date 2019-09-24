9908a45cc122c519659777bd9f8722d8ae24624e
Rafaela <rafaela@operand.com.br>
2019-09-24 10:54:20 -0300

    Created a login static layout

--- src/App.vue
+++ src/App.vue
@@ -3,3 +3,66 @@
         <router-view/>
     </div>
 </template>
+
+<style lang="scss">
+@import url('https://fonts.googleapis.com/css?family=Open+Sans|Satisfy|Source+Code+Pro&display=swap');
+html, body, #app {
+    height: 100%;
+}
+* {
+    font-family: 'Open Sans', sans-serif;
+}
+pre {
+    font-family: 'Source Code Pro', monospace !important;
+}
+$green: #0b903d;
+
+.bg-green {
+    background-color: $green !important;
+}
+.btn.focus, .btn:focus {
+    box-shadow: 0 0 0 0.2rem rgba($green, .25) !important;
+}
+.btn-link {
+    background-color: $green !important;
+    color: #fff !important;
+}
+.create-account-callout {
+    border: 1px solid #d8dee2;
+    border-radius: 5px;
+    padding: 15px 20px;
+    text-align: center;
+}
+.form-control:focus {
+    border-color: $green !important;
+    box-shadow: 0 0 0 0.2rem rgba($green, .25) !important;
+}
+.text-white {
+    color: #fff;
+    a {
+        color: #fff;
+        text-decoration: underline;
+    }
+}
+.text-30 {
+    font-size: 30px;
+}
+::-webkit-scrollbar {
+    width: 12px;
+}
+::-webkit-scrollbar:horizontal {
+    height: 12px;
+}
+::-webkit-scrollbar-track {
+    box-shadow: none;
+    background-color: rgba(#f5f5f5, .5);
+    border-radius: 12px;
+}
+::-webkit-scrollbar-thumb {
+    border-radius: 12px;
+    background-color: rgba(#ccc, .5);
+    &:hover {
+        background-color: rgba(#ccc, .9);
+    }
+}
+</style>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,6 +1,30 @@
 <template>
-    <div>
-        Login
+    <div class="d-flex flex-column justify-content-center align-items-center h-100 bg-green">
+        <div class="row w-100">
+            <div class="col col-sm-12 col-md-4 offset-md-4">
+                <div class="col text-center text-30 pb-2 text-white">
+                    Sign in to Wallet
+                </div>
+                <div class="card">
+                    <div class="card-body">
+                        <form>
+                            <div class="form-group">
+                                <label for="email">Email address</label>
+                                <input type="email" id="email" class="form-control" placeholder="Email address">
+                            </div>
+                            <div class="form-group">
+                                <label for="password">Password</label>
+                                <input type="password" id="password" class="form-control" placeholder="Password">
+                            </div>
+                            <button type="button" class="btn btn-link btn-block" >Login</button>
+                        </form>
+                    </div>
+                </div>
+                <div class="mt-2 text-center create-account-callout text-white">
+                    New to Wallet? <router-link to="/register">Create an account.</router-link>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -9,6 +33,3 @@ export default {
     name: 'Login'
 }
 </script>
-
-<style>
-</style>
