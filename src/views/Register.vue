e9fc7770023f0eaabfd51be43a21730427268b43
Rafaela <rafaela@operand.com.br>
2019-09-24 10:57:17 -0300

    Created a register static layout

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,6 +1,38 @@
 <template>
-    <div>
-        Register
+    <div class="d-flex flex-column justify-content-center align-items-center h-100 bg-green">
+        <div class="row w-100">
+            <div class="col col-sm-12 col-md-4 offset-md-4">
+                <div class="col text-center text-cursive text-30 pb-2 text-white">
+                    Register to Wallet
+                </div>
+                <div class="card">
+                    <div class="card-body">
+                        <form>
+                            <div class="form-group">
+                                <label for="name">Name</label>
+                                <input type="text" id="name" class="form-control" placeholder="Name">
+                            </div>
+                            <div class="form-group">
+                                <label for="email">Email address</label>
+                                <input type="email" id="email" class="form-control" placeholder="Email address">
+                            </div>
+                            <div class="form-group">
+                                <label for="password">Password</label>
+                                <input type="password" id="password" class="form-control" placeholder="Password">
+                            </div>
+                            <div class="form-group">
+                                <label for="password-confirm">Password Confirmation</label>
+                                <input type="password" class="form-control" id="password-confirm" placeholder="Password Confirmation">
+                            </div>
+                            <button type="button" class="btn btn-link btn-block" >Create an account</button>
+                        </form>
+                    </div>
+                </div>
+                <div class="mt-2 text-center create-account-callout text-white">
+                    You have an account? <router-link to="/login">Login.</router-link>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -9,6 +41,3 @@ export default {
     name: 'Register'
 }
 </script>
-
-<style>
-</style>
