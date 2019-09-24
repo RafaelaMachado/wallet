1ad5ffbcce4ac5284903f7e0cbbe1f65d467d9b4
Rafaela <rafaela@operand.com.br>
2019-09-24 11:13:05 -0300

    Created a wallet static layout with the divisions

--- src/views/Wallet.vue
+++ src/views/Wallet.vue
@@ -1,6 +1,18 @@
 <template>
-    <div>
-        Wallet
+    <div class="container-fluid h-100 p-0 d-flex">
+        <div class="w-25 sidebar">
+            <div class="current-user bg-light d-flex align-items-center flex-nowrap p-2">
+            </div>
+            <div class="coins-list bg-light p-2">
+            </div>
+        </div>
+
+        <div class="d-flex flex-column w-75">
+            <div class="wallet-header bg-light p-2 d-flex align-items-center">
+            </div>
+            <div class="wallet-infos flex-grow-1 p-2" ref="walletInfos">
+            </div>
+        </div>
     </div>
 </template>
 
@@ -10,5 +22,46 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
+$border-color: #c2c6ca;
+
+.current-user {
+    height: 75px;
+    font-size: 1.25rem;
+    border-bottom: 1px solid $border-color;
+}
+.fade-enter-active {
+    animation: fade-in 300ms ease-out forwards;
+}
+.fade-leave-active {
+    animation: fade-out 300ms ease-out forwards;
+}
+.sidebar {
+    border-right: 1px solid $border-color;
+}
+.wallet-header {
+    height: 75px;
+    font-size: 1.25rem;
+    border-bottom: 1px solid $border-color;
+}
+.wallet-infos {
+    height: calc(100% - 125px);
+    overflow-y: auto;
+}
+@keyframes fade-in {
+    from {
+        opacity: 0;
+    }
+    to {
+        opacity: 1;
+    }
+}
+@keyframes fade-out {
+    from {
+        opacity: 1;
+    }
+    to {
+        opacity: 0;
+    }
+}
 </style>
